e3bb78941dbfeddddcb1cf3527eb39339f307c56
caiorr1 <caiorrodrigues2004@gmail.com>
2023-05-26 23:33:44 -0300

    uptade

--- main.py
+++ main.py
@@ -4,39 +4,41 @@ import pessoafisica
 import ongs
 import empresas
 
+def main_menu():
+    loop = True
+    while loop:
+        
+        entrance = input(' Digite (1) se você é uma Empresa\n Digite (2) se você é uma ONG\n Digite (3) se você é uma Pessoa Fisíca\n Digite (4) se você é ADMIN\n')
+        entrance_list = ['1', '2', '3', '4']
+        
+        if entrance not in entrance_list:
+            print('\nOpção inválida! Tente novamente.\n')   
+            
+        
+        elif entrance == entrance_list[0]:
+            print('\nOk...Indo para cadastro de Empresas.\n')
+            empresas.get_companies()
+            loop = False
+        
+        elif entrance == entrance_list[1]:
+            print('\nOk...Indo para cadastro de ONGs.\n')  
+            ongs.get_ongs()
+            loop = False
+        
+        elif entrance == entrance_list[2]:
+            print('\nOk...Indo para cadastro de Pessoas Fisícas.\n')
+            pessoafisica.get_natural_person()
+            loop = False
+            
+        elif entrance == entrance_list[3]:
+            print('Ok...Indo para área de Admin')
+            rodarprograma.admin_run()
+            loop = False
+            
 #program start
 if __name__ == '__main__':
     
     formatacao.formatting()
     print('- Bem vindo ao SeedS, aquecendo corações! -')
     formatacao.formatting()
-
-loop = True
-while loop:
-    
-    entrance = input(' Digite (1) se você é uma Empresa\n Digite (2) se você é uma ONG\n Digite (3) se você é uma Pessoa Fisíca\n Digite (4) se você é ADMIN\n')
-    choices = ['1', '2', '3', '4']
-    
-    if entrance not in choices:
-        print('\nOpção inválida! Tente novamente\n')   
-        continue
-    
-    elif entrance == choices[0]:
-        print('\nOk...Indo para cadastro de Empresas.')
-        empresas.get_companies()
-        loop = False
-    
-    elif entrance == choices[1]:
-        print('\nOk...Indo para cadastro de ONGs')  
-        ongs.get_ongs()
-        loop = False
-    
-    elif entrance == choices[2]:
-        print('\nOk...Indo para cadastro de Pessoas Fisícas')
-        pessoafisica.get_natural_person()
-        loop = False
-        
-    elif entrance == choices[3]:
-        print('Ok...Indo para área de Admin')
-        rodarprograma.admin_run()
-        loop = False
+    main_menu()
